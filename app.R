a0b755316e890bc9a8ed5c7c050264472d0f1406
Jason Soukup <jrsoukup17@gmail.com>
2017-10-16 14:14:24 -0400

    added table; changed theme

--- app.R
+++ app.R
@@ -4,13 +4,14 @@ library(dplyr)
 library(plotly)
 library(shinythemes)
 library(RColorBrewer)
+library(DT)
 
 sal <- read.csv("Salaries.csv")
 master <- read.csv("Master.csv")
 
 ui <- fluidPage(
   
-  theme = shinytheme("superhero"),
+  theme = shinytheme("simplex"),
   HTML("<h1>Baseball Salaries: 1985-2016</h1>"),
   sidebarPanel(
     sliderInput(inputId = "year",
@@ -20,8 +21,11 @@ ui <- fluidPage(
     selectizeInput(inputId = "teamInput", "Team", choices = c("Braves", "Orioles", "Red Sox", "Angels", "White Sox", "Cubs", "Reds", "Indians", "Tigers", "Astros", "Royals", "Dodgers", "Twins", "Brewers", "Expos", "Yankees", "Mets", "Athletics", "Phillies", "Pirates", "Padres", "Mariners", "Giants", "Cardinals", "Rangers", "Blue Jays", "Rockies", "Marlins", "Diamondbacks", "Rays", "Nationals"), options= list(maxItems=30, placeholder="Add MLB Team")) 
   ),
   mainPanel(
-  plotlyOutput(outputId = "line")
+  plotlyOutput(outputId = "line"),
+  br(), br(),
+  DT::dataTableOutput("data")
   )
+  
 )
 
 server <- function(input, output) {
@@ -40,6 +44,9 @@ server <- function(input, output) {
                                        size = 2, linetype = "solid"))
     )
     })
+  output$data <- DT::renderDataTable({
+    DT::datatable(sal)
+  })
 }
 
 shinyApp(ui = ui, server = server)
